6424aa69b57cb64d057629b1634014d5ee62c0fb
Karina Mardanova <karinakereytova@gmail.com>
2022-10-21 20:50:41 +0300

    Add files via upload

---
+++ cypress/integration/my_autotests/negative_case_2.js
@@ -0,0 +1,17 @@
+
+describe('Тестирование формы авторизации', function () {
+
+    it('Негативный автотест', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('karina@gmail.com');
+        cy.get('#pass').type('iLoveqastudio2');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon')
+    })
+})
+
+
+
+
+
